d698e051f06bd52acfc9f2b12bb413924893ffcd
MyFirstRepo <adityasaah932@gmail.com>
2025-03-24 23:43:04 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const cookieParser = require("cookie-parser")
+const connectDB = require("./db/conn")
+const userroute = require("./routes/user.route")
+const usertaskroute = require("./routes/task.route")
+const roomateroute = require("./routes/roomate.route")
+const postroute = require("./routes/post.route")
+require("dotenv").config()
+
+
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use('/uploads',express.static('uploads'))
+
+connectDB();
+
+app.use("/api/v1/2024/user",userroute)
+app.use("/api/v1/2024/task",usertaskroute)
+app.use("/api/v1/2024/roomate",roomateroute)
+app.use("/api/v1/2024/post",postroute)
+
+app.listen(process.env.PORT,()=>{
+    console.log("Server running on PORT ",process.env.PORT);
+})
