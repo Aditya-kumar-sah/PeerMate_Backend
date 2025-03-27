adb5b799c7dc6068d54441263b678ec0157ff35f
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2025-03-27 22:47:14 +0530

    Update Firebase.js

--- config/Firebase.js
+++ config/Firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase_keys.json");
+const serviceAccount = require(process.env.FILEPATH);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
