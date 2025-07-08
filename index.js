545d96b82cee49d58c17eb6bd8894f15c424c8a0
Aditya Kumar Sah <141633697+Aditya-kumar-sah@users.noreply.github.com>
2025-07-08 21:25:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ require("dotenv").config()
 
 
 app.use(express.json())
+// app.use(cors({
+//   origin: "*", // Allow all origins
+//   methods: "*", // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
+//   allowedHeaders: "*", // Allow all headers
+//   credentials: true // Allow cookies, authorization headers, etc.
+// }))
 app.use(cors({
-  origin: "*", // Allow all origins
-  methods: "*", // Allow all HTTP methods (GET, POST, PUT, DELETE, etc.)
-  allowedHeaders: "*", // Allow all headers
-  credentials: true // Allow cookies, authorization headers, etc.
-}))
+  origin: 'https://peermate-frontend.onrender.com', 
+  methods: "*",         
+  allowedHeaders: "*", 
+  credentials: true                            
+}));
+
 app.use(cookieParser())
 app.use('/uploads',express.static('uploads'))
 
